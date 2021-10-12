314aac0edbd66d22053c575f2685f24d8624fafb
unknown <sndchrs440@gmail.com>
2021-10-12 18:07:33 +0530

    Add Dashboard, Sidebar and reouting

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,6 +1,10 @@
+import React from "react";
+import Sidebar from "./partial/sidebar";
+
 function Dashboard(){
     return(
         <div>
+            <Sidebar/>
             <h2>Dashboard</h2>
         </div>
     );
